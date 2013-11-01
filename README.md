3b22cf8c687a208f18f2f360086bbcd58a45d415
Recursive G <g.recursive@gmail.com>
2013-11-01 21:07:05 +0800

    bugfix

--- README.md
+++ README.md
@@ -2,15 +2,18 @@ No home in Residence
 =========
 
 A plugin for Minecraft Bukkit Server.
+
 Works with Residence & Essentials
-Add two new flags `home` and `pearl`
-When they are set to `false`, you 
-- Can't sethome in the res
+
+Add two new flags `home` and `pearl`. When they are set to `false`, you
+ 
+- Can't sethome in the res 
 - Can't sleep in the res
 - Can't Teleport into the res using `/home`
-And...
-- Can't using the Ender Pearl if the source OR destination is in the res
 
+And...
+
+- Can't using the Ender Pearl if the source OR destination is in the res
 
 Bug reports are welcome.
 
--- src/org/devinprogress/nohome/MainListener.java
+++ src/org/devinprogress/nohome/MainListener.java
@@ -2,11 +2,14 @@ package org.devinprogress.nohome;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
@@ -16,14 +19,6 @@ import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
 
 public class MainListener implements Listener{
-	@EventHandler
-	public void onEnterBed(PlayerBedEnterEvent e){
-		if (!haveHomePremit(e.getPlayer().getName(),e.getBed().getLocation())){
-			e.getPlayer().sendMessage("你不能在这里睡觉！");
-			e.setCancelled(true);
-		}
-	}
-	
 	@EventHandler
 	public void onCommandInvoke(PlayerCommandPreprocessEvent e){
 		String cmd[]=e.getMessage().split(" ");
@@ -37,6 +32,17 @@ public class MainListener implements Listener{
 		}else if(cmd[0].equalsIgnoreCase("/sethome")||cmd[0].equalsIgnoreCase("/esethome")){
 			if(!haveHomePremit(p.getName(),p.getLocation())){
 				p.sendMessage("你不能在这设置家");
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerInteractWithBed(PlayerInteractEvent e){
+		if(e.getAction()==Action.RIGHT_CLICK_BLOCK
+       	&& e.getClickedBlock().getType() == Material.BED_BLOCK){
+			if(!haveHomePremit(e.getPlayer().getName(),e.getClickedBlock().getLocation())){
+				e.getPlayer().sendMessage("你不能在这里睡觉！");
 				e.setCancelled(true);
 			}
 		}
@@ -56,19 +62,33 @@ public class MainListener implements Listener{
 	}
 	
 	private boolean homeAvailable(Player p,String h){
+		
 		try {
-			User u=((Essentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials")).getUser(p);
-			Location l=null,bed=u.getBedSpawnLocation();
-			if (h.equalsIgnoreCase("bed")||(bed!=null&&h.isEmpty())){
-				l=bed;
-			}else if(u.getHomes().size()==0)
-				return true;
-			else if(u.getHomes().size()==1)
-				l=u.getHome();
-			else
-				l=u.getHome(h);
-			
-			return haveHomePremit(p.getName(),l);
+			String t[]=h.split(":", 2);
+			if (t[0].length()==h.length()){
+				User u=((Essentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials")).getUser(p);
+				Location l=null,bed=u.getBedSpawnLocation();
+				if (h.equalsIgnoreCase("bed")||(bed!=null&&h.isEmpty())||(bed!=null&&u.getHomes().size()==0)){
+					l=bed;
+				}else if(u.getHomes().size()==0)
+					return true;
+				else if(u.getHomes().size()==1)
+					l=u.getHome();
+				else
+					l=u.getHome(h);
+				return haveHomePremit(p.getName(),l);
+			}
+			if (t.length==0)return true;
+			else{
+				h=(t[1].equals(""))?"home":t[1];
+				User u=((Essentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials")).getUser(t[0]);
+				if (u==null)return true;
+				Location l=u.getHome(h);
+				if (h.equalsIgnoreCase("bed"))l=u.getBedSpawnLocation();
+				if (u.getHomes().size()==0)l=u.getBedSpawnLocation();
+				if (l==null)return true;
+				return haveHomePremit(p.getName(),l);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
